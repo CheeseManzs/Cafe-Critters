164a7dfa57ed3c42db6275ebc6592cc936dba3cb
hoi6789 <poo_hoi@hotmail.com>
2024-08-16 05:14:34 -0400

    Update zone.gd

--- Data/Source Code/zone.gd
+++ Data/Source Code/zone.gd
@@ -6,7 +6,15 @@ extends Resource
 var name: String
 
 # what cards are in the zone
+# zones ordered like a stack: the first item in the zone is the top of this stack
 var storedCards: Array
 
-func transfer(target) -> void:
+# draws a card from target zone, useful for drawing from a hand or a graveyard
+func draw(target: Zone, cardID = 0) -> void:
+	if cardID < target.storedCards.size():
+		var selected = target.storedCards[cardID]
+	pass
+	
+func transfer(target, cardID = 0) -> void:
+	
 	pass
