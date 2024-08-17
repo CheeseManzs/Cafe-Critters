6834525d7d371be70411ff5bd91639b97897264d
CheeseMans <165634901+PokemonSearch@users.noreply.github.com>
2024-08-16 23:25:41 -0400

    Updated monster.gd

--- Data/Source Code/monster.gd
+++ Data/Source Code/monster.gd
@@ -9,8 +9,14 @@ extends Resource
 @export var statDefense: int
 @export var statAttack: int
 
-func _init(p_id = 0, p_name = "null", p_sprite = null, p_deck = null):
+func _init(p_id = 0, p_name = "null", p_sprite = null, p_deck = null, p_startingCardPool = null, 
+			p_statHealth = 0, p_statDefense = 0, p_statAttack = 0):
 	id = p_id
 	name = p_name
 	sprite = p_sprite
 	deck = p_deck
+	startingCardPool = p_startingCardPool
+	statHealth = p_statHealth
+	statDefense = p_statDefense
+	statAttack = p_statAttack
+	
