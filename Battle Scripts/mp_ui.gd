f39b364175a904a082ede3bc8edc21113db148fe
CheeseMans <36460677+CheeseManzs@users.noreply.github.com>
2024-09-04 01:07:22 -0400

    Updated Battle UI

--- Battle Scripts/mp_ui.gd
+++ Battle Scripts/mp_ui.gd
@@ -1,14 +1,32 @@
 extends VBoxContainer
 @export var battleController: BattleController
+@export var mpFill: TextureProgressBar
 @export var mpText: RichTextLabel
 @export var player = false
+var oldMP = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func textPop() -> void:
+	scale = Vector2(1.7, 1.7)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	scale = lerp(scale, Vector2(1, 1),delta*16)
 	mpText.text = "[center]"+str(battleController.playerMP)+"[/center]"
+	var dt: float = battleController.playerMP - mpFill.value
+	var normalDT: float = dt/abs(dt)
+	mpFill.value += normalDT*delta
+
+	if abs(mpFill.value - battleController.playerMP) < delta*2:
+		mpFill.value = battleController.playerMP
+	else:
+		print("filling: ",normalDT*delta)
+		mpFill.value += normalDT*delta
+	
+	if oldMP != battleController.playerMP:
+		oldMP = battleController.playerMP
+		textPop()
