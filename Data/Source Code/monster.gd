b7cf62b6f3982112a0afd0bc0c07bc710526cf10
hoi6789 <poo_hoi@hotmail.com>
2024-09-28 01:07:30 -0400

    Update monster.gd

--- Data/Source Code/monster.gd
+++ Data/Source Code/monster.gd
@@ -9,6 +9,7 @@ enum ALIGNMENT {
 	Sec,
 	Eco,
 	Jacks,
+	Kress,
 	Blanc
 }
 
@@ -19,6 +20,7 @@ enum ROLE {
 	Support
 }
 
+# fixed features of the monster.
 @export var id: int
 @export var name: String
 @export var alignment: ALIGNMENT
@@ -28,17 +30,23 @@ enum ROLE {
 @export var passive: PassiveAbility
 @export var deck: Zone
 
+# variables related to what cards they have and gain access to.
 @export var startingCardPool: Zone
 @export var levelupCards: Array[Zone]
 
+# strings that store info about their stat growth curves.
 @export var rawHealth: String
 @export var rawAttack: String
 @export var rawDefense: String
+@export var levelingType: String
+@export var levelingItems: Array[String]
 
+# Depreciated
 @export var statHealth: Array
 @export var statAttack: Array
 @export var statDefense: Array
 
+# level
 @export var level: int
 
 @export var battleOffset: Vector2
@@ -47,6 +55,19 @@ var StatCurves = {
 	"growth40": [40, 46, 52, 58, 65, 71, 79, 86, 93, 100],
 	"growth15": [15, 16, 18, 19, 21, 22, 24, 26, 28, 30],
 	"growth12": [12, 13, 14, 16, 17, 19, 20, 22, 23, 24]
+}
+
+# Level costs are an array with multiple layers.
+# Layer 1: 9 entries, holds the definitions for each level.
+# Layer 2: holds how many unique ingredients are require for each level.
+# Layer 3: defines each individual ingredient. [0] is count, [1] is index of levelingItems.
+var LevelCosts = {
+	"basic": [[ [2, 0] ], [ [3, 0] ], [ [4, 0] ], [ [2, 1], [2, 0] ], [ [3, 1], [3, 0] ],\
+			 [ [4, 1], [5, 0] ], [ [2, 2], [3, 1], [3, 0] ], [ [3, 2], [4, 1], [5, 0] ],\
+			 [ [4, 2], [5, 1], [8, 0] ]],
+	"elemental": [[ [2, 0] ], [ [2, 0], [2, 1] ], [ [3, 0], [3, 1] ], [ [2, 2], [4, 1] ],\
+			 [ [3, 2], [2, 3] ], [ [4, 2], [3, 3], [3, 1] ], [ [2, 4], [5, 3], [5, 1] ], \
+			 [ [3, 4], [2, 5], [5, 3] ], [ [4, 4], [4, 5] ]],
 }
 
 func _init(p_id = 0, p_name = "null", p_sprite = null, p_deck = null, p_startingCardPool = null, 
