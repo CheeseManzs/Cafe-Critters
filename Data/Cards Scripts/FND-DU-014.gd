6fce82e25494e360395bfa310ac97fe25968a482
CheeseMans <36460677+CheeseManzs@users.noreply.github.com>
2025-01-26 18:15:27 -0500

    Added Lemon-Aid

--- Data/Cards Scripts/FND-DU-014.gd
+++ Data/Cards Scripts/FND-DU-014.gd
@@ -6,10 +6,14 @@ func _init() -> void:
 	alignment = ALIGNMENT.Default
 	role = "Haatea"
 	description = "Gain Barrier 10. Gain Regen 2."
-	name = "Lemon-AId"
+	name = "Lemon-Aid"
 
-func effect(attacker: BattleMonster, defender: BattleMonster) -> int:
-	pass
+func effect(attacker: BattleMonster, defender: BattleMonster):
+	await giveStatus(attacker,Status.EFFECTS.BARRIER,10)
+	await giveStatus(attacker,Status.EFFECTS.REGEN,2)
 
-func calcShield(attacker: BattleMonster, defender: BattleMonster) -> int:
-	pass
+func calcStatusGiven(attacker: BattleMonster, defender: BattleMonster) -> Status:
+	var lv = 10
+	if statusConditions.has(Status.EFFECTS.EMPOWER):
+		lv = 15
+	return Status.new(Status.EFFECTS.BARRIER, lv)
