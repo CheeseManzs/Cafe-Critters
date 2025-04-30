0f177372e72180ecb085fd3712292d3c81056238
hoi6789 <poo_hoi@hotmail.com>
2025-04-29 23:44:48 -0400

    fixed deck edit ui crash bugs
    still not done deck editor yet. tune in next time

--- Overworld Scripts/deck_edit_ui.gd
+++ Overworld Scripts/deck_edit_ui.gd
@@ -1,3 +1,7 @@
+# Main script for handling deck editor logic.
+# Needs to be ported into the Overworld script. Will figure that out soon
+# Click on cards (right) to add them into decks (left)
+
 extends Control
 @export var storedCards: CardStorage = CardStorage.new()
 @export var monDeck: CardStorage = CardStorage.new()
@@ -5,6 +9,7 @@ extends Control
 @export var enemyMons: Array[Monster]
 var allCards: Array[Card]
 var allMonsters: Array[Monster]
+var storedID: int
 
 var cardObject : PackedScene = load("res://Prefabs/card_object.tscn")
 
@@ -12,19 +17,29 @@ var cardObject : PackedScene = load("res://Prefabs/card_object.tscn")
 func _ready() -> void:
 	buildCards()
 	buildMonsters()
+	rebuildCards()
+	# sets up the buttons that let you choose monster slots
 	for i in range(6):
 		var temp = Button.new()
 		temp.text = "monster" + str(i + 1)
+		# stores the monster's "id" as a meta variable, which then gets
+		# retrieved by the onclick to be used later! clever
 		temp.set_meta("id", i)
 		temp.pressed.connect(_monster_button_pressed.bind(temp.get_meta("id")))
-		$ScrollContainer/Control/Panel.add_child(temp)
-	var id = 0
+		%MonsterButtons.add_child(temp)
+	
+	# sets up a "change monster" button
+	var temp = Button.new()
+	temp.text = "change monster"
+	temp.pressed.connect(toggleMonsters.bind())
+	%MonsterButtons.add_child(temp)
+	
+	# sets up an extra button for each monster in Data/Monsters
 	for monster in allMonsters:
-		var temp = Button.new()
+		temp = Button.new()
 		temp.text = monster.name
-		temp.pressed.connect(_monster_select_pressed.bind(id))
-		id += 1
-		$Panel/MonsterGridContainer.add_child(temp)
+		temp.pressed.connect(_monster_select_pressed.bind(monster))
+		$MonsterSelectPanel/MonsterGridContainer.add_child(temp)
 	pass # Replace with function body.
 
 
@@ -32,6 +47,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass 
 
+# finds every monster implemented so far and stores them to allMonsters for future
+# stores Monster resources which we can plug diretly into teams (nice)
 func buildMonsters():
 	var monsterDir = DirAccess.open("res://Data/Monsters")
 	monsterDir.list_dir_begin()
@@ -41,6 +58,8 @@ func buildMonsters():
 		readMonsterName = monsterDir.get_next()
 	pass
 
+# finds every card made so far and stores them into allCards for future
+# stores Card resources which plug directly into decks
 func buildCards():
 	var cardDir = DirAccess.open("res://Data/Cards")
 	cardDir.list_dir_begin()
@@ -49,6 +68,9 @@ func buildCards():
 		allCards.push_back(load("res://Data/Cards/" + readCardName))
 		readCardName = cardDir.get_next()
 		
+# used to add cards to the cards display
+# currently shows all cards (TODO: ADD FILTERING)
+# it's the collection so you click here to add to the deck uhhh
 func rebuildCards(): 
 	for item in allCards:
 		var temp = cardObject.instantiate()
@@ -60,6 +82,8 @@ func rebuildCards():
 		cParent.add_child(temp)
 		%RightGridContainer.add_child(cParent)
 		
+# stupid function name
+# loads the deck of a selected monster onto the left
 func rebuildMonsters(id):
 	var team
 	if id > 2: 
@@ -67,16 +91,20 @@ func rebuildMonsters(id):
 		id -= 3
 	else:
 		team = playerMons 
-	for item in team[id].deck:
-		var temp = cardObject.instantiate()
-		var cParent = Control.new()
-		temp.displayLocation = "collection"
-		temp.setCard(item, 1, null, "collection")
-		temp.deckEditController = self
-		temp.set_meta("half", "left")
-		cParent.add_child(temp)
-		%LeftGridContainer.add_child(cParent)
-		pass
+	if team[id] != null:
+		for child in %LeftGridContainer.get_children():
+			child.queue_free()
+		if team[id].deck.storedCards.size() == 0:
+			team[id].deck.storedCards = team[id].startingCardPool.storedCards
+		for item in team[id].deck.storedCards:
+			var temp = cardObject.instantiate()
+			var cParent = Control.new()
+			temp.displayLocation = "collection"
+			temp.setCard(item, 1, null, "collection")
+			temp.deckEditController = self
+			temp.set_meta("half", "left")
+			cParent.add_child(temp)
+			%LeftGridContainer.add_child(cParent)
 		
 	#for i in range(storedCards.itemNames.size()):
 	#	for j in range(storedCards.itemNames[i].size()):
@@ -85,8 +113,28 @@ func rebuildMonsters(id):
 	#		var temp = storedCards.instantiate()
 	#		temp.initiate(storedCards.cardNames[i][j], storedCards.cardData[i][j][1])
 
+func toggleMonsters():
+	$MonsterSelectPanel.visible = !$MonsterSelectPanel.visible
+
+# selects a monster slot and loads their deck if possible so you can edit it
 func _monster_button_pressed(id):
+	storedID = id
+	rebuildMonsters(storedID)
 	pass
 	
-func _monster_select_pressed(id):
+# sets a monster into a selected monster slot
+# ids 0-2 are player team, ids 3-5 are enemy team
+func _monster_select_pressed(storedMonster):
+	var team
+	var internalID
+	if storedID > 2: 
+		team = enemyMons
+		internalID = storedID - 3
+	else:
+		team = playerMons 
+		internalID = storedID
+	team[internalID] = storedMonster
+	%MonsterButtons.get_child(storedID).text = "monster" + str(storedID + 1) + " (" + storedMonster.name + ")"
+	rebuildMonsters(storedID)
+	$MonsterSelectPanel.visible = false
 	pass
