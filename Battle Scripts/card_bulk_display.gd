de47758c38b11ecad30e85aa1b083fb328156131
CheeseMans <36460677+CheeseManzs@users.noreply.github.com>
2025-06-02 00:39:27 -0400

    Updated Bulk Display Prototype

--- Battle Scripts/card_bulk_display.gd
+++ Battle Scripts/card_bulk_display.gd
@@ -7,9 +7,11 @@ extends Control
 static var display_scale = 0.8
 
 func addCardDisplay(cardDisplay: CardDisplay):
+	var subContainer: Control = Control.new()
 	#subContainer.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	#cardDisplay.custom_minimum_size = cardDisplay.size*0.5
 	var oldSize = cardDisplay.size
-	cardContainer.add_child(cardDisplay)
+	cardContainer.add_child(subContainer)
+	subContainer.add_child(cardDisplay)
 	cardDisplay.custom_minimum_size = oldSize
 	cardDisplay.scaleFactor = 1
